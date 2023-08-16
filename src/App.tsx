4670632ccda801ea2cf5a64ee70e9e717590f1f7
TulioRibeiroS <44611234+TulioRibeiroS@users.noreply.github.com>
2023-08-16 13:37:03 -0300

    Adicionando o vercel/analytics

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Home from "./pages/home";
 import CardGrid from "./pages/list";
 import Form from "./pages/form";
 import Header from "./Components/Header";
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="list-task" element={<CardGrid />} />
         <Route path="add-task" element={<Form />} />
       </Routes>
+      <Analytics />
     </BrowserRouter>
   );
 }
